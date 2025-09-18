1be87aa836660a41d19773562e4f96652c433c0f
Blessed <kimaniblessed545@gmail.com>
2025-09-18 15:54:16 +0300

    usermanagement UI

--- models/db.php
+++ models/db.php
@@ -40,5 +40,15 @@
 
      }
        
+//generates a random string of specified length
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!$&#?%(){}[]:*_';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 ?>
